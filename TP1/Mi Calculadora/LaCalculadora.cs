8ad0a1a47b38045cacdb766b74cff6a1f99e51a8
geralsdepp <geraldinemeza23@gmail.com>
2018-04-05 00:33:20 -0300

    Add files via upload

---
+++ TP1/Mi Calculadora/LaCalculadora.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TP1;
+
+namespace Mi_Calculadora
+{
+    public partial class LaCalculadora : Form
+    {
+        public LaCalculadora()
+        {
+            InitializeComponent();
+            this.Text = "Calculadora de Geraldine del curso 2C";
+            this.CenterToScreen();
+            MinimizeBox = false;
+            MaximizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            cmbOperador.Items.Add("+");
+            cmbOperador.Items.Add("-");
+            cmbOperador.Items.Add("*");
+            cmbOperador.Items.Add("/");
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            Numero num = new Numero(txtNumero1.Text);
+            lblResultado.Text = num.DecimalBinario(txtNumero1.Text);
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Numero num = new Numero(txtNumero1.Text);
+            lblResultado.Text = num.BinarioDecimal(txtNumero1.Text);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado = LaCalculadora.Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+            lblResultado.Text = resultado.ToString();
+        }
+
+        //Borrar los datos del text box, comboBox y Label.
+        private void Limpiar()
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            cmbOperador.ResetText();
+            lblResultado.ResetText();
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            double resultado = 0;
+            Calculadora calculadora = new Calculadora();
+            Numero num1 = new Numero(numero1);
+            Numero num2 = new Numero(numero2);
+            resultado = calculadora.Operar(num1, num2, operador);
+
+            return resultado;
+        }
+    }
+}
---
+++ TP1/TP1/Numero.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    public class Numero
+    {
+        private double numero;
+
+        private string SetNumero
+        { 
+            set
+            {
+                this.numero = this.ValidarNumero(value);
+            }
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            string retorno = "Valor invalido";
+
+            double aux = 0;
+            int x = 1;
+            string numdec = "";
+
+            //for para revertir la cadena
+            for (int i = binario.Length - 1; i >= 0; i--)
+            {
+                numdec += binario[i];
+            }
+
+            //for para calcular el numero decimal
+            for (int i = 0; i < numdec.Length; i++)
+            {
+                x = Convert.ToInt32(numdec[i]);
+                //si el caracter es 1 calcular potencia
+                if (x == 49)
+                    aux += Math.Pow(2, i);
+            }
+            //convirtiendo a string para mostrar # decimal
+            if (aux>0)
+	        {
+		        retorno = aux.ToString();
+	        }
+            
+            return retorno;
+        }
+        public string DecimalBinario(double numero)
+        {
+            string retorno = "";
+            if (numero > 0)
+            {
+                while (numero > 0)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        retorno = "0" + retorno;
+                    }
+                    else
+                    {
+                        retorno = "1" + retorno;
+                    }
+                    numero = (int)numero / 2;
+                }
+            }
+            else if(numero == 0)
+            {               
+                retorno = "0"; 
+            }
+            else
+            {
+                retorno = "Valor invalido";
+            }
+            
+            return retorno;
+        }
+        public string DecimalBinario(string numero)
+        {
+            string retorno = "";
+            double aux;
+            if (double.TryParse(numero, out aux))
+            {
+                retorno = this.DecimalBinario(aux);
+            }
+            else
+                retorno = "Valor invalido";
+            return retorno;
+        }
+        public Numero(){}
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+        public Numero(string strNumero)
+        {
+            this.SetNumero = strNumero;
+        }
+        public static double operator -(Numero n1, Numero n2)
+        {
+            double retorno = n1.numero - n2.numero;
+            return retorno;
+        }
+        public static double operator +(Numero n1, Numero n2)
+        {
+            double retorno = n1.numero + n2.numero;
+            return retorno;
+        }
+        public static double operator /(Numero n1, Numero n2)
+        {
+            double retorno = n1.numero / n2.numero;
+            return retorno;
+        }
+        public static double operator *(Numero n1, Numero n2)
+        {
+            double retorno = n1.numero * n2.numero;
+            return retorno;
+        }
+        public static bool operator !=(Numero n2, int cero)
+        {
+            bool retorno = false;
+
+            if (n2.numero != cero)
+                retorno = true;
+            return retorno;
+        }
+        public static bool operator ==(Numero n2, int cero)
+        {
+            return !(n2.numero != cero);
+        }
+        private double ValidarNumero(string strNumero)
+        {
+            double retorno = 0;
+            int num;
+            if(Int32.TryParse(strNumero, out num))
+                retorno = (double)num;
+            return retorno;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
