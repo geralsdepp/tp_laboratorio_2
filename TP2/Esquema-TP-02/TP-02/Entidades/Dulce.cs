2f83f1f0ddc7151726ecae8fbb3b84b5b409e33c
geralsdepp <geraldinemeza23@gmail.com>
2018-05-16 02:48:54 -0300

    Add files via upload

---
+++ TP2/Esquema-TP-02/TP-02/Entidades/Dulce.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string codigoDeBarras, ConsoleColor color)
+            : base(marca, codigoDeBarras, color) { }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("");
+            sb.AppendFormat("CALORIAS : {0}\n", this.CantidadCalorias);
+            
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
