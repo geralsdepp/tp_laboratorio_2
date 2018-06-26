9cd7663141d144e74e14018fa623784b5c0b6589
geralsdepp <geraldinemeza23@gmail.com>
2018-06-25 22:13:26 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP1/TP1/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    public class Calculadora
+    {
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double retorno = -1;
+            switch (operador)
+            {
+                case "+":
+                    retorno = num1 + num2;
+                    break;
+                case "-":
+                    retorno = num1 - num2;
+                    break;
+                case "*":
+                    retorno = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        retorno = num1 / num2;
+                    }
+                    break;
+            }
+            return retorno;
+        }
+
+        private static string ValidarOperador(string operador)
+        {
+            string retorno = "+";
+            switch (operador)
+            {
+                case "+":
+                    retorno = operador;
+                    break;
+                case "-":
+                    retorno = operador;
+                    break;
+                case "*":
+                    retorno = operador;
+                    break;
+                case "/":
+                    retorno = operador;
+                    break;
+            }
+            return retorno;
+        }
+    }
+}
